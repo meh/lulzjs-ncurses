7dfd4bd32bb3bbd50753c37ea7afbcd37ebd54d7
meh <meh.ffff@gmail.com>
2009-01-25 23:07:29 +0100

    Little addition to the test :P

--- test/panel.js
+++ test/panel.js
@@ -6,10 +6,11 @@ var screen = ncurses.Screen.init({buffering: ncurses.Buffering.Raw});
 var panels = []
 panels.push(new ncurses.Panel({x: 2, y: 2, width: 30, height: 10, border: true}));
 panels.push(new ncurses.Panel({x: 4, y: 5, width: 30, height: 10, border: true}));
-panels[0].printString("Width: {Width}, Height: {Height}".format(panels[0].Size), {x: 1, y: 1});
+panels[0].printString("Width: {Width}, Height: {Height}   ".format(panels[0].Size), {x: 1, y: 1});
 panels[1].printString("PENIS", {x: 10, y: 4});
 panels[0].toTop();
 panels[0].getChar();
 panels[0].below.getChar();
 panels[0].resize({height: 1});
+panels[0].printString("Width: {Width}, Height: {Height}   ".format(panels[0].Size), {x: 1, y: 1});
 panels[0].getChar();
